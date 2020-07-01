82eacc2c1c4c55ebb5224467a909c25cc1e74745
Isaac Molina <isaacmolina.m16@gmail.com>
2020-06-30 20:32:12 -0500

    added clearRect method

--- myscript.js
+++ myscript.js
@@ -6,6 +6,8 @@ var dx = 2;
 var dy = -2;
 
 function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //rect method takes in four parameters
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI*2, false);
     //arc method takes in parameters, used for making circle
