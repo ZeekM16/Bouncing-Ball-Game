b5daf9d646d38e0fb7fc8f086e4a0e406792e39a
Isaac Molina <isaacmolina.m16@gmail.com>
2020-06-30 20:23:29 -0500

    ball movement

--- myscript.js
+++ myscript.js
@@ -1,15 +1,21 @@
 var canvas = document.getElementById("mycanvas");
 var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height -30;
+var dx = 2;
+var dy = -2;
 
 function draw() {
     ctx.beginPath();
-    ctx.arc(220,140,20,0,Math.PI*2,false);
+    ctx.arc(x, y, 10, 0, Math.PI*2, false);
     //arc method takes in parameters, used for making circle
     //false makes circle counter clockwise
     ctx.fillStyle = "red";
     //gives color to ball
     ctx.fill();
     ctx.closePath();
+    x +=dx;
+    y +=dy;
 
 }
 
